1a16a41cddaa0e6cbea0480c08034d898f7d445d
Steven Roper <steven.m.roper@gmail.com>
2015-02-04 19:39:36 -0700

    Redoing jquery overlay solution according to how Amy-Kate did it.

--- js/app.js
+++ js/app.js
@@ -2,19 +2,26 @@
 //Solution: Create an overlay with the large image - Lightbox
 $(document).ready(function() {
 	
+	var $overlay = $('<div id="overlay"></div>')
+	$('body').append($overlay);
+
+
+	/* MY SOLUTION
 	//Capture click event on image
 	$('#imageGallery a').click(function( event ) {
 		
 		//Prevent new page from opening on click
 		event.preventDefault();
 		
+	
+
 		//Store image URL & image caption for overlay use
 		var $clickedImageURL = $(this).children().attr('src');
 		var $clickedImageCaption = $(this).children().attr('alt');
 
 		//Add image & caption to overlay div
-		$('#overlay').html('<img src=\'' + $clickedImageURL + '\'>' + '<p>' + $clickedImageCaption + '</p>');
-		
+		//$('#overlay').html('<img src=\'' + $clickedImageURL + '\'>' + '<p>' + $clickedImageCaption + '</p>');
+
 		//Fade in overlay
 		$('#overlay').fadeIn('fast');
 	});
@@ -22,6 +29,34 @@ $(document).ready(function() {
 	//Hide overlay once user clicks on overlay
 	$('#overlay').click( function() {
 		$('#overlay').fadeOut('fast');
+	*/
+
+
+	//CLASS SOLUTION
+
+	//Show overlay w/ image & caption when image is clicked
+	$('#imageGallery a').click(function( event ) {
+		
+		//Prevent new page from opening on click
+		event.preventDefault();
+
+		//Store image URL & image caption for overlay use
+		var $clickedImageURL = $(this).children('img').attr('src');
+		var $clickedImageCaption = $(this).children('img').attr('alt');
+
+		//Add image and caption to overlay
+		$overlay.append('<img src="' + $clickedImageURL + '">');
+		$overlay.append('<p>' + $clickedImageCaption + '</p>');
+
+		//Fade in overlay
+		$overlay.fadeIn('fast');
 	});
+
+	//Hide overlay when screen is clicked
+	$($overlay).on('click', function() {
+		$overlay.empty();
+		$overlay.fadeOut('fast');
+	});
+
 });
 
